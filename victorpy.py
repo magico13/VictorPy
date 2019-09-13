c5abb64431457eff1a6f433c8932fcd8d657ee62
magico13 <magico1313@gmail.com>
2019-09-12 22:10:55 -0400

    Finally able to read/write some data

--- victorpy.py
+++ victorpy.py
@@ -4,6 +4,10 @@ import time
 
 vector = None
 
+readUUID = "30619f2d-0f54-41bd-a65a-7588d8c85b45"
+writeUUID = "7d2a4bda-d29b-4152-b725-2491478c5cd7"
+cccd = "00002902-0000-1000-8000-00805f9b34fb"
+
 class BLEDelegate(btle.DefaultDelegate, object):
     def handleNotification(self, cHandle, data):
         print('handleNotification: cHandle: {0} data: {1}'.format(cHandle, data))
@@ -22,7 +26,9 @@ class BLEDelegate(btle.DefaultDelegate, object):
                 global vector
                 vector = scanEntry
       
-
+def printBytes(bytes):
+    print(','.join(str(b) for b in bytes))
+        
 
 scanner = btle.Scanner()
 scanner.clear()
@@ -42,19 +48,54 @@ if not vector:
 else: print('Connecting to {0}'.format(vector.getValueText(9)))
 dev = btle.Peripheral(vector.addr, vector.addrType)
 
-print("Services...")
+
+
+print("Services:")
 for svc in dev.getServices():
-    print(str(svc))
-    print("Characteristics: ")
+    print('-'+str(svc))
+    print("--Descriptors:")
+    for desc in svc.getDescriptors():
+        val = '<No Read Support>'
+        try:
+            val = desc.read()
+        except: pass
+        print('---{0}: {1}'.format(desc, val))
+    print("--Characteristics: ")
     for char in svc.getCharacteristics():
         val = '<No Read Support>'
         if char.supportsRead(): val = str(char.read())
-        print(str(char)+': '+val)
+        print('---'+str(char)+': '+val)
+
 comSvc = dev.getServiceByUUID('fee3')
+readChar = comSvc.getCharacteristics(readUUID)[0]
+writeChar = comSvc.getCharacteristics(writeUUID)[0]
+cccdDesc = comSvc.getDescriptors(cccd)[0]
+
+cccdDesc.write(bytes([1, 0]))
+
+# read the current value
+data = None
 while True:
-    for char in comSvc.getCharacteristics():
-        val = '<No Read Support>'
-        if char.supportsRead(): val = str(char.read())
-        print(str(char)+': '+val)
+    data = readChar.read()
+    printBytes(data)
+    if len(data) > 0: break
     time.sleep(1)
+    
+# write the bytes '197,1,5,0,0,0'
+b = bytes([197,1,5,0,0,0])
+writeChar.write(b)
+
+lastData = data
+while True:
+    data = readChar.read()
+    if len(data) > 0 and data != lastData:
+        lastData = data
+        printBytes(data)
+    time.sleep(0.1)
+#print('Waiting 10s for notifications')
+#attempts = 0
+#while attempts < 10:
+#    if dev.waitForNotifications(1): continue
+#    attempts += 1
+
   
